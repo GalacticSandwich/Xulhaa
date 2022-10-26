01a99ba2f4c09dc2ef320c4e480bc5849d617e70
Aaron N <95148185+GalacticSandwich@users.noreply.github.com>
2022-10-26 17:10:52 -0400

    Add files via upload

---
+++ meta/load.js
@@ -0,0 +1,31 @@
+
+var navbarHTML = `
+    <p class="nv-div">~~~~~</p>
+    <br/>
+    <p class="nv-comp">HOME</p>
+    <p class="nv-comp">INFORMATION</p>
+    <br/>
+    <h4 class="nv-head">ENVIRONMENT</h4>
+    <p class="nv-comp">GEOGRAPHY</p>
+    <p class="nv-comp">HISTORY</p>
+    <p class="nv-comp">CULTURE</p>
+    <h4 class="nv-head">LANGUAGE</h4>
+    <p class="nv-comp">OVERVIEW</p>
+    <p class="nv-comp">PHONOLOGY</p>
+    <p class="nv-comp">ORTHOGRAPHY</p>
+    <p class="nv-comp">SYNTAX</p>
+    <p class="nv-comp">NOUNS</p>
+    <p class="nv-comp">PRONOUNS</p>
+    <p class="nv-comp">VERBS</p>
+    <p class="nv-comp">POSTPOSITIONS</p>
+    <p class="nv-comp">ADJECTOIDS</p>
+    <h4 class="nv-head">RESOURCES</h4>
+    <p class="nv-comp">DICTIONARY</p>
+    <p class="nv-comp">WRITINGS</p>
+    <br/>
+    <p class="nv-div">~~~~~</p>
+`;
+
+window.onload = function() {
+    document.getElementById("navbar").innerHTML = navbarHTML;
+}
