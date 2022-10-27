6fcfc63a7e807978fc5076cc3e8badc246275992
Aaron N <95148185+GalacticSandwich@users.noreply.github.com>
2022-10-27 14:55:07 -0400

    Add files via upload

--- meta/load.js
+++ meta/load.js
@@ -1,27 +1,34 @@
 
 var navbarHTML = `
+    <img class="nv-icon" src="/Xulhaa/media/xulhaa-icon.png">
+    <br/>
     <p class="nv-div">~~~~~</p>
     <br/>
-    <p class="nv-comp">HOME</p>
-    <p class="nv-comp">INFORMATION</p>
+    <p class="nv-comp"><a class="nv-link" href="#">HOME</a></p>
+    <p class="nv-comp"><a class="nv-link" href="#">INFORMATION</a></p>
+    <p class="nv-comp"><a class="nv-link" href="#">DEVELOPMENT</a></p>
+    <p class="nv-comp"><a class="nv-link" href="#">UPDATES</a></p>
     <br/>
     <h4 class="nv-head">ENVIRONMENT</h4>
-    <p class="nv-comp">GEOGRAPHY</p>
-    <p class="nv-comp">HISTORY</p>
-    <p class="nv-comp">CULTURE</p>
+    <p class="nv-comp"><a class="nv-link" href="#">HISTORY</a></p>
+    <p class="nv-comp"><a class="nv-link" href="#">CULTURE</a></p>
+    <p class="nv-comp"><a class="nv-link" href="#">GEOGRAPHY</a></p>
     <h4 class="nv-head">LANGUAGE</h4>
-    <p class="nv-comp">OVERVIEW</p>
-    <p class="nv-comp">PHONOLOGY</p>
-    <p class="nv-comp">ORTHOGRAPHY</p>
-    <p class="nv-comp">SYNTAX</p>
-    <p class="nv-comp">NOUNS</p>
-    <p class="nv-comp">PRONOUNS</p>
-    <p class="nv-comp">VERBS</p>
-    <p class="nv-comp">POSTPOSITIONS</p>
-    <p class="nv-comp">ADJECTOIDS</p>
+    <p class="nv-comp"><a class="nv-link" href="#">OVERVIEW</a></p>
+    <p class="nv-comp"><a class="nv-link" href="#">EVOLUTION</a></p>
+    <p class="nv-comp"><a class="nv-link" href="#">PHONOLOGY</a></p>
+    <p class="nv-comp"><a class="nv-link" href="#">ORTHOGRAPHY</a></p>
+    <p class="nv-comp"><a class="nv-link" href="#">GRAMMAR</a></p>
+    <p class="nv-comp"><a class="nv-link" href="#">SYNTAX</a></p>
+    <p class="nv-comp"><a class="nv-link" href="#">NOUNS</a></p>
+    <p class="nv-comp"><a class="nv-link" href="#">PRONOUNS</a></p>
+    <p class="nv-comp"><a class="nv-link" href="#">VERBS</a></p>
+    <p class="nv-comp"><a class="nv-link" href="#">POSTPOSITIONS</a></p>
+    <p class="nv-comp"><a class="nv-link" href="#">ADJECTOIDS</a></p>
     <h4 class="nv-head">RESOURCES</h4>
-    <p class="nv-comp">DICTIONARY</p>
-    <p class="nv-comp">WRITINGS</p>
+    <p class="nv-comp"><a class="nv-link" href="#">DICTIONARY</a></p>
+    <p class="nv-comp"><a class="nv-link" href="#">WRITINGS</a></p>
+    <p class="nv-comp"><a class="nv-link" href="#">GALLERY</a></p>
     <br/>
     <p class="nv-div">~~~~~</p>
 `;
